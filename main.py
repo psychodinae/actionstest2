a2931b0e23cb0217543e1594e2d6bf1fa6aa21b1
mellon_collie <67171012+psychodinae@users.noreply.github.com>
2022-10-09 05:14:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from time import sleep
+import datetime
+import requests
+
+
+while True:
+    print (f"ping {datetime.date.today()}"}
+    requests.get("https://hookb.in/VGplaap1z6TDebxDwjrZ")
+    sleep(60)
+
 
